5cb534a7609d3e0d9eeb17fa5e1a2928c952101e
Naga Vijayapuram <nvijayap@gmail.com>
2026-04-15 13:53:39 +0530

    Expanded llm/run.sh

--- llm/run.sh
+++ llm/run.sh
@@ -6,8 +6,25 @@ if [ $# -lt 2 ]; then
   printf "\nNeed Args: <Model> <Prompt>\n\n"; exit 1
 fi
 
-echo; nc -zv 127.0.0.1 11434 || brew services run ollama
-sleep 1; nc -zv 127.0.0.1 11434
+args="$@"; model=$1
 
-ollama ls | grep -q ^$1 || ollama pull $1 >/dev/null 2>&1
-go run main.go "$@"; echo; echo
+curl -s http://localhost:11434/api/tags | \
+  jq -r .models.[].model >/tmp/models
+
+grep -q $model /tmp/models
+
+if [ $? -ne 0 ]; then
+  printf "\n$model is not present in https://ollama.com/library\n"
+  printf "\nThese are the models that exist in the library ...\n\n"
+  cat -n /tmp/models; echo; exit
+fi
+
+echo; nc -zv 127.0.0.1 11434 || \
+  brew services run ollama
+
+sleep 1; nc -zv 127.0.0.1 11434 >/dev/null 2>&1
+
+ollama ls | grep -q ^$1 || \
+  ollama pull $1 >/dev/null 2>&1
+
+go run main.go $args; echo; echo
