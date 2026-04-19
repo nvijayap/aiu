317b19d59ad51246eac5cd88c802c3d543de93c4
Naga Vijayapuram <nvijayap@gmail.com>
2026-04-19 16:21:30 +0530

    run.sh - Moved the placement of a check

--- llm/run.sh
+++ llm/run.sh
@@ -8,6 +8,10 @@ fi
 
 args="$@"; model=$1
 
+echo; nc -zv 127.0.0.1 11434 || \
+  brew services run ollama
+sleep 1; nc -zv 127.0.0.1 11434 >/dev/null 2>&1
+
 curl -s http://localhost:11434/api/tags | \
   jq -r .models.[].model >/tmp/models
 
@@ -18,11 +22,6 @@ if [ $? -ne 0 ]; then
   printf "\nThese are the models that exist in the library ...\n\n"
   cat -n /tmp/models; echo; exit
 fi
-
-echo; nc -zv 127.0.0.1 11434 || \
-  brew services run ollama
-
-sleep 1; nc -zv 127.0.0.1 11434 >/dev/null 2>&1
 
 ollama ls | grep -q ^$1 || \
   ollama pull $1 >/dev/null 2>&1
